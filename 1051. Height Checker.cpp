38d15d92fd79e9807ed5b502aa5a6c7b2d5f6b1e
Asadjafri9 <144949213+Asadjafri9@users.noreply.github.com>
2024-06-11 01:54:45 +0500

    Create 1051. Height Checker.cpp

---
+++ 1051. Height Checker.cpp
@@ -0,0 +1,16 @@
+https://leetcode.com/problems/height-checker/submissions/1284104880?envType=daily-question&envId=2024-06-10
+
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        int count = 0;
+        vector<int> store = heights;
+      
+        sort(store.begin(), store.end());
+        for (int i = 0; i < heights.size(); i++) {
+            if (store[i] != heights[i])
+                count++;
+        }
+        return count;
+    }
+};
