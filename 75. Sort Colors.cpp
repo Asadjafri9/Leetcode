92f74ec22b15a37acffd5f9ea1131364c817029d
Asadjafri9 <144949213+Asadjafri9@users.noreply.github.com>
2024-06-12 15:34:27 +0500

    75. Sort Colors.cpp

---
+++ 75. Sort Colors.cpp
@@ -0,0 +1,7 @@
+https://leetcode.com/problems/sort-colors/submissions/1285838462
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+    }
+};
